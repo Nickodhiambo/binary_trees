85276062ef556ac59d67bc0a36804a9f338d520a
Nick <nodhiambo01@gmail.com>
2023-08-31 06:31:30 -0700

    Fixed betty codestyle errors

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -10,7 +10,7 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return;
-	
+
 	/*Uses post-order traversal to delete nodes*/
 	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->right);
