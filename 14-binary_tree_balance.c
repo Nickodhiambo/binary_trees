1ad196fbc201b03d33721e80de37679f364dc25d
Nick <nodhiambo01@gmail.com>
2023-08-31 23:30:38 -0700

    Update

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -18,12 +18,40 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree->left)
 	{
-		left_height += 1 + binary_tree_balance(tree->left);
+		left_height += 1 + binary_tree_height(tree->left);
 	}
 	if (tree->right)
 	{
-		right_height += 1 + binary_tree_balance(tree->right);
+		right_height += 1 + binary_tree_height(tree->right);
 	}
 	bf = left_height - right_height;
 	return (bf);
+}
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node
+ * Return: Tree height
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0;
+	size_t right_height = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left)
+	{
+		left_height += 1 + binary_tree_height(tree->left);
+	}
+	if (tree->right)
+	{
+		right_height += 1 + binary_tree_height(tree->right);
+	}
+	if (left_height > right_height)
+		return (left_height);
+	else
+		return (right_height);
 }
