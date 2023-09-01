95250d0651d04a0ee88b2036761bbbc686774d31
Nick <nodhiambo01@gmail.com>
2023-08-31 23:23:06 -0700

    Update

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,9 +9,9 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t bf;
-	size_t left_height = 0;
-	size_t right_height = 0;
+	int bf;
+	int left_height = 0;
+	int right_height = 0;
 
 	if (tree == NULL)
 		return (0);
